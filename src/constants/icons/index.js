5cec60e7244e6116124869cc8507bcbc5763af3e
Sandun Jayawardhana <99672222+SandunJay@users.noreply.github.com>
2024-12-17 16:37:57 +0000

    icon fix

--- src/constants/icons/index.js
+++ src/constants/icons/index.js
@@ -1,36 +1,73 @@
-import css from './css.svg'
-import express from './express.svg'
-import git from './git.svg'
-import github from './github.svg'
-import html from './html.svg'
-import java from './java.svg'
-import javascript from './javascript.svg'
-import mongodb from './mongodb.svg'
-import csharp from './csharp.svg'
-import motion from './motion.svg'
-import mui from './mui.svg'
-import nextjs from './nextjs.svg'
-import nodejs from './nodejs.svg'
-import react from './react.svg'
-import redux from './redux.svg'
-import sass from './sass.svg'
-import tailwindcss from './tailwindcss.svg'
-import typescript from './typescript.svg'
-import linkedin from './linkedin.svg'
-import twitter from './twitter.svg'
-import car from './car.svg'
-import estate from './estate.svg'
-import pricewise from './pricewise.svg'
-import snapgram from './snapgram.svg'
-import summiz from './summiz.svg'
-import threads from './threads.svg'
-import arrow from './arrow.svg'
-import contact from './contact.svg'
-import soundon from './soundon.png'
-import soundoff from './soundoff.png'
-import python from './python.svg'
-import django from './django.svg'
-import sqlite from './sqlite.svg'
+// import css from './css.svg'
+// import express from './express.svg'
+// import git from './git.svg'
+// import github from './github.svg'
+// import html from './html.svg'
+// import java from './java.svg'
+// import javascript from './javascript.svg'
+// import mongodb from './mongodb.svg'
+// import csharp from './csharp.svg'
+// import motion from './motion.svg'
+// import mui from './mui.svg'
+// import nextjs from './nextjs.svg'
+// import nodejs from './nodejs.svg'
+// import react from './react.svg'
+// import redux from './redux.svg'
+// import sass from './sass.svg'
+// import tailwindcss from './tailwindcss.svg'
+// import typescript from './typescript.svg'
+// import linkedin from './linkedin.svg'
+// import twitter from './twitter.svg'
+// import car from './car.svg'
+// import estate from './estate.svg'
+// import pricewise from './pricewise.svg'
+// import snapgram from './snapgram.svg'
+// import summiz from './summiz.svg'
+// import threads from './threads.svg'
+// import arrow from './arrow.svg'
+// import contact from './contact.svg'
+// import soundon from './soundon.png'
+// import soundoff from './soundoff.png'
+// import python from './python.svg'
+// import django from './django.svg'
+// import sqlite from './sqlite.svg'
+
+import apachejmeter from './apachejmeter.svg';
+import arrow from './arrow.svg';
+import car from './car.svg';
+import contact from './contact.svg';
+import csharp from './csharp.svg';
+import css from './css.svg';
+import django from './django.svg';
+import estate from './estate.svg';
+import express from './express.svg';
+import git from './git.svg';
+import github from './github.svg';
+import html from './html.svg';
+import java from './java.svg';
+import javascript from './javascript.svg';
+import linkedin from './linkedin.svg';
+import mongodb from './mongodb.svg';
+import motion from './motion.svg';
+import mui from './mui.svg';
+import mysql from './mysql.svg';
+import nextjs from './nextjs.svg';
+import nodejs from './nodejs.svg';
+import pricewise from './pricewise.svg';
+import python from './python.svg';
+import react from './react.svg';
+import redux from './redux.svg';
+import sass from './sass.svg';
+import selenium from './selenium.svg';
+import snapgram from './snapgram.svg';
+import soundoff from './soundoff.png';
+import soundon from './soundon.png';
+import sqlite from './sqlite.svg';
+import summiz from './summiz.svg';
+import tailwindcss from './tailwindcss.svg';
+import threads from './threads.svg';
+import twitter from './twitter.svg';
+import typescript from './typescript.svg';
 
 export {
     django,
